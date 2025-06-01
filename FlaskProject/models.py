96b1cb538d3bca03947ef05b51270d34730d4a37
EmmaMan23 <emma.j_man23@outlook.com>
2025-06-01 10:57:31 +0100

    Models created for database

---
+++ FlaskProject/models.py
@@ -0,0 +1,44 @@
+from app import db
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)  # Store hashed passwords 
+    role = db.Column(db.String(50), nullable=False)       # e.g., 'admin', 'user'
+    marked_for_deletion = db.Column(db.Boolean, default=False) #user can mark fo deletion
+
+    def __repr__(self):
+        return f'<Users {self.username} ({self.role})>'
+    
+class Listing(db.Model):
+    listing_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    author = db.Column(db.String(255))
+    description = db.Column(db.String (400))
+    genre_id = db.Column(db.Integer, db.ForeignKey('genre.genre_id'))
+    is_available = db.Column(db.Boolean, default=True)
+    marked_for_deletion = db.Column(db.Boolean, default=False) #user can mark fo deletion
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    user = db.relationship('User', backref='listings')
+    genre = db.relationship('Genre', backref='listings')
+    
+
+    def __repr__(self):
+        return f"<Listing {self.title} by User {self.listed_by} (Available: {self.is_available})>"
+    
+class Loan(db.Model):
+    loan_id = db.Column(db.Integer, primary_key=True)
+    listing_id = db.Column(db.Integer, db.ForeignKey('listing.listing_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    start_date = db.Column(db.Date)
+    return_date = db.Column(db.Date)
+    is_returned = db.Column(db.Boolean, default=True)
+    user = db.relationship('User', backref='loans')
+    listing = db.relationship('Listing', backref='loans')
+
+
+class Genre(db.Model):
+    genre_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    image = db.Column(db.String(255), nullable=True)
+    marked_for_deletion = db.Column(db.Boolean, default=False) #user can mark fo deletion
