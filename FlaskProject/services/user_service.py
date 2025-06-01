f836e08446d3c7041f7291e5c804e2635c3cc6a4
Emrozah <123465100+Emrozah@users.noreply.github.com>
2025-06-01 16:52:10 +0100

    Added registration logic

---
+++ FlaskProject/services/user_service.py
@@ -0,0 +1,33 @@
+#from werkzeug.security import generate_password_hash, check_password_hash
+from models import User
+from utils import Result
+from extensions import db
+
+
+def register_user(form):
+    
+
+    username = form.get('username')
+    password = form.get('password')
+    re_password = form.get('re_password')
+    user_type = form.get('user_type')
+    
+    if not username or not password or not re_password:
+        return Result(False, "All fields are required.")
+    
+    if password != re_password:
+        return Result(False, "Passwords do not match!")
+    
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        return Result(False, "username already taken")
+    
+    new_user = User(username=username, role=user_type)
+    new_user.set_password(password)
+
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return Result(True)
+    
