d4ae859cb93a9a3efcc8ff49e36d2eb7a02a7230
EmmaMan23 <emma.j_man23@outlook.com>
2025-06-01 16:52:10 +0100

    Added registration logic

--- FlaskProject/routes/auth.py
+++ FlaskProject/routes/auth.py
@@ -1,9 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from flask import redirect, url_for, flash
+from services import user_service
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+
+    if request.method == 'POST':
+        form_data = request.form
+        #print("Received POST:", form_data)
+        result = user_service.register_user(form_data)
+
+        if result.success:
+            flash("Registration successful, please log in", "success")
+            return redirect(url_for('auth.login'))
+        else:
+            flash(result.message, "danger")
+            return render_template('login.html') #keep showing register form if registration failed
+        
     return render_template('login.html')
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+    return render_template('login.html')
+
 
---
+++ FlaskProject/utils.py
@@ -0,0 +1,5 @@
+
+class Result:
+    def __init__(self, success, message=None):
+        self.success = success
+        self.message = message
