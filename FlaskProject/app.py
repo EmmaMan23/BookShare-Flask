8c38d07da991655d4cf7da43c1ffbb9c4dfc359a
EmmaMan23 <emma.j_man23@outlook.com>
2025-06-02 16:33:09 +0100

    Added some login required features

--- FlaskProject/app.py
+++ FlaskProject/app.py
@@ -25,6 +25,7 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+    login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
     @login_manager.user_loader
