a1eb8cfa997c4bd25131e1e3f29e8ef28a2211aa
Emrozah <123465100+Emrozah@users.noreply.github.com>
2025-06-02 16:33:09 +0100

    Added some login required features

--- FlaskProject/routes/dashboard.py
+++ FlaskProject/routes/dashboard.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template, request
 from flask import redirect, url_for, flash
+from flask_login import login_required, current_user
+
 
 dash = Blueprint('dash', __name__)
 
 @dash.route('/dashboard')
+@login_required
 def dashboard():
-    return("Hello dashbaord")
+    return render_template('dashboard.html')
