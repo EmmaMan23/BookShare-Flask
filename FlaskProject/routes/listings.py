36f6b5c245a989476878ada1a9276dcb94d8ac08
EmmaMan23 <emma.j_man23@outlook.com>
2025-06-04 15:43:23 +0100

    Form for create a listing done

--- FlaskProject/routes/listings.py
+++ FlaskProject/routes/listings.py
@@ -1,13 +1,24 @@
 from flask import Blueprint, render_template, request
 from flask import redirect, url_for, flash
 from flask_login import login_required, current_user
+from models import Genre
+from services import listing_service
 
 listings = Blueprint ('listings', __name__)
 
 @listings.route('/create_listing', methods=['POST', 'GET'])
 @login_required
 def create_listing():
-    return("List books here")
+
+    genres = Genre.query.filter_by(inactive=False).all()
+
+    if request.method == 'POST':
+        form_data = request.form
+        result = listing_service.list_book(form_data)
+        
+        #print(request.form.get('Title'))
+
+    return render_template('create_listing.html', genres=genres)
 
 @listings.route('/view_listings')
 @login_required
